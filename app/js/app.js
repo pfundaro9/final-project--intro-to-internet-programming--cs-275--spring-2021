9a39859da93d49548efd7a74ae13066ba086f21a
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-05-12 03:19:57 -0400

    Added Animated Diamond

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,29 @@
 window.onload = () => {
     let diamondSize = prompt("Please enter the size of the diamond");
-    let diamondSizeInt = parseInt(diamondSize);
-    console.log(diamondSizeInt + 20);
+//    let diamondSizeInt = parseInt(diamondSize);
+    let diamondContainer1 = document.getElementById(`diamondContainer`);
+    if (isNaN(diamondSize) || diamondSize === "" ){
+        alert("That is not a number. Please try again");
+    }else {
+        let diamondSizeInt = parseInt(diamondSize);
+    for (let i = 0; i <= diamondSizeInt; i++){
+        for (let j = 1; j <= diamondSizeInt - i; j++){
+            diamondContainer1.innerHTML += "&nbsp;&nbsp";
+        }
+        for (let j = 1; j <= 2*i-1; j++){
+            diamondContainer1.innerHTML += "*";
+        }
+        diamondContainer1.innerHTML += "<br>";
+    }
+    for (let i = diamondSizeInt - 1; i >= 1; i--){
+        for (let j = 1; j <= diamondSizeInt - i; j++){
+            diamondContainer1.innerHTML += "&nbsp;&nbsp";
+        }
+        for (let j = 1; j <= 2*i-1; j++){
+            diamondContainer1.innerHTML += "*";
+        }
+        diamondContainer1.innerHTML += "<br>";
+    }
+    diamondContainer1.classList.toggle("diamondContainer");
+}
 };
